f16ae3885ad24b5d774f90d2ed13e27b921c630a
Maksim Klippa <mklippa@ozon.ru>
2019-09-11 09:47:09 +0300

    init

---
+++ src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login">
+    <h2>Sign In</h2>
+    <input
+      type="text"
+      placeholder="Email"
+    /> <br />
+    <input
+      type="password"
+      placeholder="Password"
+    />
+    <br />
+    <button>Connection</button>
+    <p>You don't have an accoutn? You can create one</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>

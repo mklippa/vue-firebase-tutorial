96b0ec1a1e71a7a400d5ba3a1068b63421a8b051
root <root@nb-msk-a0050.ozon>
2019-09-12 12:50:03 +0300

    firebase integration

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyBrIde-g4mZsLqqjIeysXvpu2MSCmxpZ6E',
+  authDomain: 'vue-firebase-tutorial-41978.firebaseapp.com',
+  databaseURL: 'https://vue-firebase-tutorial-41978.firebaseio.com',
+  projectId: 'vue-firebase-tutorial-41978',
+  storageBucket: '',
+  messagingSenderId: '371766639690',
+  appId: '1:371766639690:web:25ce9e08fa13f3da8f7ab3'
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
 new Vue({
   router,

d9117013506178ae7bf6bb3637f6ef56c2076bca
Maksim Klippa <mklippa@ozon.ru>
2019-09-12 08:57:45 +0300

    Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -8,8 +8,7 @@
     <input
       type="password"
       placeholder="Password"
-    />
-    <br />
+    /> <br />
     <button>Connection</button>
     <p>You don't have an accoutn? You can create one</p>
   </div>
@@ -24,3 +23,27 @@ export default {
   methods: {}
 }
 </script>
+
+<style scoped>
+.login {
+  margin-top: 40px;
+}
+input {
+  margin: 10px 0;
+  width: 20%;
+  padding: 15px;
+}
+button {
+  margin-top: 20px;
+  width: 20%;
+  cursor: pointer;
+}
+p {
+  margin-top: 40px;
+  font-size: 13px;
+}
+p a {
+  text-decoration: underline;
+  cursor: pointer;
+}
+</style>
